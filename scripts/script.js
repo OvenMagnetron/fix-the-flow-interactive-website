02f02a770d194545b69bf51fe012f8c161260e7a
OvenMagnetron <jessekramer15@gmail.com>
2023-09-27 20:56:07 +0200

    Cleanup, mooier zichtbaar

--- scripts/script.js
+++ scripts/script.js
@@ -31,7 +31,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const carousel = document.querySelector(".carousel");
   const card = carousel.querySelector(".card");
   const cardWidth = card.offsetWidth;
-  const numCardsToShow = 2; // Aantal kaarten om te laten zien bij elke klik
+  const numCardsToShow = 1; // Aantal kaarten om te laten zien bij elke klik
   const arrowBtns = document.querySelectorAll(".wrapper button");
 
   // Bereken de breedte van de kaarten die getoond moeten worden
